e3677535db69f8e6c805623a8d977c141f298510
davi.rocha <davi.rocha@intercase.com.br>
2019-09-25 17:33:20 -0300

    create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { HeaderStyled } from './style'
+
+const Header = () => ( 
+    <HeaderStyled>
+      <header>
+        REACT<span>JS</span>
+      </header>
+    </HeaderStyled>
+);
+
+export default Header;
